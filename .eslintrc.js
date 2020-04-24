09260c72304ca325bcc0cf84ae7f41cb3d599934
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-04-25 02:24:25 +0900

    modify mock data.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    "@typescript-eslint/no-var-requiress": 0,
+    "@typescript-eslint/no-explicit-any": 0,
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
--- mocks/api/login.ts
+++ mocks/api/login.ts
@@ -1,10 +1,9 @@
-const data = require('../data/login.json')
+// typescript-eslint-disable-next-line @typescript-eslint/no-var-requires
+// const data = require("../data/login.json");
+import data from "../data/login.json";
 
 export default {
   post(): any {
-    return [
-      200,
-      data
-    ]
+    return [200, data];
   }
-}
+};
